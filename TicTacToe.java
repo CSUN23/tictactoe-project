e985dd63c1dc9e9903b31a63b11271792ec3c2c6
CSUN23 <guba.atoa@gmail.com>
2023-03-28 09:00:55 +0800

    updated names

--- TicTacToe.java
+++ TicTacToe.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 public class TicTacToe {
     public static void main(String[] args) {
-        int cnt = 0;
-        String[][] array = {
+        int turns = 0;
+        String[][] board = {
                 {" ", "1", "2", "3"},
                 {"1", "-", "-", "-"},
                 {"2", "-", "-", "-"},
@@ -10,19 +10,19 @@ public class TicTacToe {
         };
         for (int i = 0; i <= 3; i++) {
             for (int j = 0; j <= 3; j++) {
-                System.out.print(array[i][j] + " ");
+                System.out.print(board[i][j] + " ");
             }
             System.out.println();
         }
         boolean win = false;
         while (!win) {
             System.out.println("'X', choose your location (row, column)");
-            cnt++;
+            turns++;
             int row, column;
             Scanner scanner = new Scanner(System.in);
             row = scanner.nextInt();
             column = scanner.nextInt();
-            while (array[row][column].equals("X") || array[row][column].equals("O")) {
+            while (board[row][column].equals("X") || board[row][column].equals("O")) {
                 System.out.println("Invalid. Enter again.");
                 System.out.println("'X', choose your location (row, column)");
                 row = scanner.nextInt();
@@ -34,48 +34,48 @@ public class TicTacToe {
                 row = scanner.nextInt();
                 column = scanner.nextInt();
             }
-            array[row][column] = "X";
+            board[row][column] = "X";
             for (int i = 0; i <= 3; i++) {
                 for (int j = 0; j <= 3; j++) {
-                    System.out.print(array[i][j] + " ");
+                    System.out.print(board[i][j] + " ");
                 }
                 System.out.println();
             }
-            if (array[1][1].equals("X") && array[1][2].equals("X") && array[1][3].equals("X")) {
+            if (board[1][1].equals("X") && board[1][2].equals("X") && board[1][3].equals("X")) {
                 System.out.println("'X' wins");
                 break;
-            } else if (array[1][1].equals("X") && array[2][2].equals("X") && array[3][3].equals("X")) {
+            } else if (board[1][1].equals("X") && board[2][2].equals("X") && board[3][3].equals("X")) {
                 System.out.println("'X' wins");
                 break;
-            } else if (array[1][3].equals("X") && array[2][2].equals("X") && array[3][1].equals("X")) {
+            } else if (board[1][3].equals("X") && board[2][2].equals("X") && board[3][1].equals("X")) {
                 System.out.println("'X' wins");
                 break;
-            } else if (array[2][1].equals("X") && array[2][2].equals("X") && array[2][3].equals("X")) {
+            } else if (board[2][1].equals("X") && board[2][2].equals("X") && board[2][3].equals("X")) {
                 System.out.println("'X' wins");
                 break;
-            } else if (array[3][1].equals("X") && array[3][2].equals("X") && array[3][3].equals("X")) {
+            } else if (board[3][1].equals("X") && board[3][2].equals("X") && board[3][3].equals("X")) {
                 System.out.println("'X' wins");
                 break;
-            } else if (array[1][1].equals("X") && array[2][1].equals("X") && array[3][1].equals("X")) {
+            } else if (board[1][1].equals("X") && board[2][1].equals("X") && board[3][1].equals("X")) {
                 System.out.println("'X' wins");
                 break;
-            } else if (array[1][2].equals("X") && array[2][2].equals("X") && array[3][2].equals("X")){
+            } else if (board[1][2].equals("X") && board[2][2].equals("X") && board[3][2].equals("X")){
                 break;
-            } else if (array[1][3].equals("X") && array[2][3].equals("X") && array[3][3].equals("X")){
+            } else if (board[1][3].equals("X") && board[2][3].equals("X") && board[3][3].equals("X")){
                 break;
             }
             
             //game tie
-            if(cnt==9){
+            if(turns==9){
                 System.out.println("It's a tie!");
                 break;
             }
             //O
             System.out.println("'O', choose your location (row, column)");
-            cnt++;
+            turns++;
             row = scanner.nextInt();
             column = scanner.nextInt();
-            while (array[row][column].equals("X") || array[row][column].equals("O")) {
+            while (board[row][column].equals("X") || board[row][column].equals("O")) {
                 System.out.println("Invalid. Enter again.");
                 System.out.println("'O', choose your location (row, column)");
                 row = scanner.nextInt();
@@ -87,34 +87,34 @@ public class TicTacToe {
                 row = scanner.nextInt();
                 column = scanner.nextInt();
             }
-            array[row][column] = "O";
+            board[row][column] = "O";
             for (int i = 0; i <= 3; i++) {
                 for (int j = 0; j <= 3; j++) {
-                    System.out.print(array[i][j] + " ");
+                    System.out.print(board[i][j] + " ");
                 }
                 System.out.println();
             }
-            if (array[1][1].equals("O") && array[1][2].equals("O") && array[1][3].equals("O")) {
+            if (board[1][1].equals("O") && board[1][2].equals("O") && board[1][3].equals("O")) {
                 System.out.println("'O' wins");
                 break;
-            } else if (array[1][1].equals("O") && array[2][2].equals("O") && array[3][3].equals("O")) {
+            } else if (board[1][1].equals("O") && board[2][2].equals("O") && board[3][3].equals("O")) {
                 System.out.println("'O' wins");
                 break;
-            } else if (array[1][3].equals("O") && array[2][2].equals("O") && array[3][1].equals("O")) {
+            } else if (board[1][3].equals("O") && board[2][2].equals("O") && board[3][1].equals("O")) {
                 System.out.println("'O' wins");
                 break;
-            } else if (array[2][1].equals("O") && array[2][2].equals("O") && array[2][3].equals("O")) {
+            } else if (board[2][1].equals("O") && board[2][2].equals("O") && board[2][3].equals("O")) {
                 System.out.println("'O' wins");
                 break;
-            } else if (array[3][1].equals("O") && array[3][2].equals("O") && array[3][3].equals("O")) {
+            } else if (board[3][1].equals("O") && board[3][2].equals("O") && board[3][3].equals("O")) {
                 System.out.println("'O' wins");
                 break;
-            } else if (array[1][1].equals("O") && array[2][1].equals("O") && array[3][1].equals("O")) {
+            } else if (board[1][1].equals("O") && board[2][1].equals("O") && board[3][1].equals("O")) {
                 System.out.println("'O' wins");
                 break;
-            } else if (array[1][2].equals("O") && array[2][2].equals("O") && array[3][2].equals("O")){
+            } else if (board[1][2].equals("O") && board[2][2].equals("O") && board[3][2].equals("O")){
                 break;
-            } else if (array[1][3].equals("O") && array[2][3].equals("O") && array[3][3].equals("O")){
+            } else if (board[1][3].equals("O") && board[2][3].equals("O") && board[3][3].equals("O")){
                 break;
             }
         }
